63ee5731ee5afe8779de2c01f41df8874dd5a016
glyh <lyhokia@gmail.com>
2025-06-17 15:26:50 +0800

    Tracks release commits

--- scripts/update_release_info.sh
+++ scripts/update_release_info.sh
@@ -12,8 +12,10 @@ function usage() {
     fi
     cat << EOF
 Usage: ${0##*/} [--devnet-tag <new_devnet_tag>] [--mainnet-tag <new_mainnet_tag>]
-  --devnet-tag     The new devnet tag to use
-  --mainnet-tag    The new mainnet tag to use
+  --mainnet-tag       The new mainnet tag to use
+  --devnet-tag        The new devnet tag to use
+  --mainnet-commit    The new mainnet commit to use
+  --devnet-commit     The new devnet commit to use
 
 This script takes old release info, scan through the whole repo and replace old
 tags them with provided new tags.
@@ -21,20 +23,27 @@ EOF
     exit 1
 }
 
+HAS_WORK=0
+
 while [[ "$#" -gt 0 ]]; do case $1 in
-  --devnet-tag) NEW_DEVNET_TAG="$2"; shift;;
-  --mainnet-tag) NEW_MAINNET_TAG="$2"; shift;;
+  --mainnet-tag) NEW_MAINNET_TAG="$2"; HAS_WORK=1; shift;;
+  --devnet-tag) NEW_DEVNET_TAG="$2"; HAS_WORK=1; shift;;
+  --mainnet-commit) NEW_MAINNET_COMMIT="$2"; HAS_WORK=1; shift;;
+  --devnet-commit) NEW_DEVNET_COMMIT="$2"; HAS_WORK=1; shift;;
   *) usage "Unknown parameter passed: $1";;
 esac; shift; done
 
-if [[ -z "$NEW_DEVNET_TAG" && -z "$NEW_MAINNET_TAG" ]]; then
+if [[ "$HAS_WORK" -eq 0 ]]; then
   usage "Nothing to update."
 fi
 
-# Unified function: handles reading, logging, and replacing
-process_tag() {
+process_update() {
   local json_key="$1"
   local new_value="$2"
+
+  if [ -z "$new_value" ]; then
+    return 0
+  fi
 
   local old_value
   old_value=$(jq -r "$json_key" ./metadata.json)
@@ -55,6 +64,10 @@ process_tag() {
 }
 
 echo "Replacing release tags"
-[ -z "$NEW_MAINNET_TAG"] || process_tag '.release.tags.mainnet' "$NEW_MAINNET_TAG"
-[ -z "$NEW_DEVNET_TAG"] || process_tag '.release.tags.devnet' "$NEW_DEVNET_TAG"
+
+process_update '.release.tags.mainnet' "$NEW_MAINNET_TAG"
+process_update '.release.tags.devnet' "$NEW_DEVNET_TAG"
+process_update '.release.commits.mainnet' "$NEW_MAINNET_COMMIT"
+process_update '.release.commits.devnet' "$NEW_DEVNET_COMMIT"
+
 echo "Replacement complete, please check if there's any false positives"
